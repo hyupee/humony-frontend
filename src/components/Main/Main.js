f3c7fd9427945cd0226e410e2d7d9b1d902ec860
Alvin <sanghup123@naver.com>
2019-07-23 19:14:09 +0900

    스타일 추가

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,42 @@ import React from 'react';
 import * as MainSection from '../Section';
 import { Link } from 'react-router-dom';
 import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActionArea from '@material-ui/core/CardActionArea';
+
+import CardImage from '../../assets/images/3.jpeg';
+import CardImage2 from '../../assets/images/jfl.jpg';
+import smileImage from '../../assets/images/smile.jpg';
+
+const useStyles = makeStyles({
+  card: {
+    padding: 5,
+    maxWidth: 345,
+    height: 300,
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    margin: 'auto',
+    transform: 'rotate(10deg)'
+  },
+  card2: {
+    padding: 5,
+    maxWidth: 345,
+    height: 300,
+    position: 'absolute',
+    top: 55,
+    bottom: 0,
+    right: 30,
+    margin: 'auto'
+  },
+});
 
 
 const Main = () => {
-  const sectionList = [
-    { component: '' }
-  ];
+  const classes = useStyles();
 
   return (
     <>
@@ -21,7 +51,28 @@ const Main = () => {
         <div className="section_title_box">
           <h2 className="section_title">내가 원하는 부분만<br />골라서 다운로드!</h2>
           <p className="section_title_sub">단 한번의 업로드로 빠르고 정확하게!<br />내가 원하는 부분만 다운로드할 수 있어요</p>
+
+
+          <Card className={classes.card}>
+            <CardMedia
+              component="img"
+              alt="Contemplative Reptile"
+              height="290"
+              image={CardImage}
+              title="Contemplative Reptile"
+            />
+          </Card>
+          <Card className={classes.card2}>
+            <CardMedia
+              component="img"
+              alt="Contemplative Reptile"
+              height="290"
+              image={CardImage2}
+              title="Contemplative Reptile"
+            />
+          </Card>
         </div>
+        
       </MainSection.Section2>
       <MainSection.Section3>
         <div className="section_title_box ta-r">
